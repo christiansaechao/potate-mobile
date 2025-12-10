520682133c6f5f7e9d5b0c805b4a741c3b37b50a
Akhilan Jeyaraj <zatorakhilan@gmail.com>
2025-12-10 16:53:43 -0500

    it should work now lmao

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,20 @@
+// metro.config.js
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
+const path = require("node:path");
 
-const config = getDefaultConfig(__dirname);
+// Load Expo's default config first
+let config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: "./global.css" });
+// Add your alias AND preserve Expo Router
+config.resolver.extraNodeModules = {
+  ...config.resolver.extraNodeModules,
+  "@": path.resolve(__dirname),
+};
+
+// Wrap with NativeWind
+config = withNativeWind(config, {
+  input: "./global.css",
+});
+
+module.exports = config;
