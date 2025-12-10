0ac39813b5a41e5fdc93ac36e0b459db01eced5c
Calvin Fronda <cfronda209@gmail.com>
2025-12-09 16:05:04 -0800

    feat: drizzle studio;

--- db/client.ts
+++ db/client.ts
@@ -2,6 +2,6 @@ import { drizzle } from "drizzle-orm/expo-sqlite";
 import { openDatabaseSync } from "expo-sqlite";
 import * as schema from "./schema";
 
-const expo = openDatabaseSync("db.db");
+export const expo_db = openDatabaseSync("db.db");
 
-export const db = drizzle(expo, { schema });
+export const db = drizzle(expo_db, { schema });
