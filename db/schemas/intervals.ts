6d861044e2190c8f34fd56637ac88915911451fa
Young Song <youngsong1230@gmail.com>
2025-12-11 21:42:49 -0800

    g

--- db/schemas/intervals.ts
+++ db/schemas/intervals.ts
@@ -15,7 +15,9 @@ import { sessions } from "./sessions";
 
 export const intervals = sqliteTable("intervals", {
   id: integer("id").primaryKey({ autoIncrement: true }),
-  sessionId: integer("session_id").references(() => sessions.id).notNull(),
+  sessionId: integer("session_id")
+    .references(() => sessions.id)
+    .notNull(),
   startTime: integer("start_time").notNull(),
   endTime: integer("end_time"),
 });
