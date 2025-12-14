525b153beb2618fc3156d0ff7c50244851e790b0
christiansaechao <62818511+christiansaechao@users.noreply.github.com>
2025-12-13 17:53:01 -0800

    fixed issue with the font not loading into application

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,8 +4,9 @@ import {
   ThemeProvider as NavigationThemeProvider,
 } from "@react-navigation/native";
 import { useDrizzleStudio } from "expo-drizzle-studio-plugin";
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
 import "react-native-reanimated";
 import "../global.css";
 
@@ -20,22 +21,36 @@ import { db, expo_db } from "../db/client";
 import migrations from "@/drizzle/migrations";
 import { useMigrations } from "drizzle-orm/expo-sqlite/migrator";
 
+import { useFonts } from "expo-font";
+
 export const unstable_settings = {
   anchor: "(tabs)",
 };
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+
+  const [loaded] = useFonts({
+    Nunito: require("../assets/fonts/Nunito-Bold.ttf"),
+    Baloo: require("../assets/fonts/Baloo2-Bold.ttf"),
+  });
+
   useDrizzleStudio(expo_db);
 
   const { success, error } = useMigrations(db, migrations);
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
 
   if (error) {
     return <CustomText>DB failed to initialize</CustomText>;
   }
 
   if (!success) {
-    return <CustomText>Setting up database...</CustomText>;
+    return <CustomText>Setting up...</CustomText>;
   }
 
   return (
