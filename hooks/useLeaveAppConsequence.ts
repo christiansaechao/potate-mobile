69725f439d029327181f6bed5c6db528e29ce715
Young Song <youngsong1230@gmail.com>
2025-12-26 10:14:14 -0800

    lint

--- hooks/useLeaveAppConsequence.ts
+++ hooks/useLeaveAppConsequence.ts
@@ -6,89 +6,89 @@ import { TimerMode, TimerState } from "../types/types";
 import userOps from "@/lib/settings";
 
 export const useLeaveAppConsequence = (
-    state: TimerState,
-    setHealth: Dispatch<React.SetStateAction<number>>,
-    setState: Dispatch<React.SetStateAction<TimerState>>,
-    setTimeLeft: Dispatch<React.SetStateAction<number>>,
-    fetchQuote: any,
-    mode: TimerMode,
-    setExp: Dispatch<React.SetStateAction<number>>
+  state: TimerState,
+  setHealth: Dispatch<React.SetStateAction<number>>,
+  setState: Dispatch<React.SetStateAction<TimerState>>,
+  setTimeLeft: Dispatch<React.SetStateAction<number>>,
+  fetchQuote: any,
+  mode: TimerMode,
+  setExp: Dispatch<React.SetStateAction<number>>
 ) => {
-    const healthRef = useRef<number | null>(null);
-    const notificationRef = useRef<string | null>(null);
-    const deathRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+  const healthRef = useRef<number | null>(null);
+  const notificationRef = useRef<string | null>(null);
+  const deathRef = useRef<ReturnType<typeof setTimeout> | null>(null);
 
-    useEffect(() => {
-        const subscription = AppState.addEventListener(
-            "change",
-            async (nextAppState: AppStateStatus) => {
-                if (nextAppState === "background" && state === TimerState.RUNNING) {
-                    //   setState(TimerState.PAUSED);
-                    // 30 second timer here
-                    let countdown = 45;
-                    deathRef.current = setInterval(() => {
-                        countdown--;
-                        console.log("death countdown:", countdown);
-                        if (countdown <= 0) {
-                            console.log("potato dead")
-                            if (deathRef.current) clearInterval(deathRef.current);
-                            setHealth(0);
-                            setState(TimerState.IDLE);
-                            setTimeLeft(DEFAULT_TIMES[mode]);
-                            // Reset exp on death
-                            setExp(0);
-                            userOps.updateUserSettings({ exp: 0 });
-                        }
-                    }, 1000); // every 1 second
-
-                    // 15 second notif here
-                    notificationRef.current =
-                        await Notifications.scheduleNotificationAsync({
-                            content: {
-                                title: "HEY STOP!",
-                                body: "You have 30 seconds left until your potato dies, go back!",
-                            },
-                            trigger: {
-                                type: Notifications.SchedulableTriggerInputTypes.TIME_INTERVAL,
-                                seconds: 15,
-                                repeats: false,
-                            },
-                        });
-
-                    // start damage
-                    if (healthRef.current) clearInterval(healthRef.current);
-                    healthRef.current = setInterval(() => {
-                        setHealth((prev: number) => Math.max(0, prev - 5));
-                    }, 1000 * 10);
-                } else if (nextAppState === "active") {
-                    // stop damage
-                    if (healthRef.current) clearInterval(healthRef.current);
-                    // stop death timer
-                    if (deathRef.current) clearInterval(deathRef.current);
-                    // stop notificaiton timer
-                    if (notificationRef.current) {
-                        Notifications.cancelScheduledNotificationAsync(
-                            notificationRef.current
-                        );
-                    }
-
-                    setHealth((prev: number) => {
-                        if (prev < 80 && state === TimerState.RUNNING) {
-                            fetchQuote(mode, state, prev);
-                        }
-                        return prev;
-                    });
-                }
+  useEffect(() => {
+    const subscription = AppState.addEventListener(
+      "change",
+      async (nextAppState: AppStateStatus) => {
+        if (nextAppState === "background" && state === TimerState.RUNNING) {
+          //   setState(TimerState.PAUSED);
+          // 30 second timer here
+          let countdown = 45;
+          deathRef.current = setInterval(() => {
+            countdown--;
+            console.log("death countdown:", countdown);
+            if (countdown <= 0) {
+              console.log("potato dead");
+              if (deathRef.current) clearInterval(deathRef.current);
+              setHealth(0);
+              setState(TimerState.IDLE);
+              setTimeLeft(DEFAULT_TIMES[mode]);
+              // Reset exp on death
+              setExp(0);
+              userOps.updateUserSettings({ exp: 0 });
             }
-        );
+          }, 1000); // every 1 second
 
-        return () => {
-            subscription.remove();
-            if (healthRef.current) clearInterval(healthRef.current);
-            if (deathRef.current) clearInterval(deathRef.current);
-            if (notificationRef.current) {
-                Notifications.cancelScheduledNotificationAsync(notificationRef.current);
+          // 15 second notif here
+          notificationRef.current =
+            await Notifications.scheduleNotificationAsync({
+              content: {
+                title: "HEY STOP!",
+                body: "You have 30 seconds left until your potato dies, go back!",
+              },
+              trigger: {
+                type: Notifications.SchedulableTriggerInputTypes.TIME_INTERVAL,
+                seconds: 15,
+                repeats: false,
+              },
+            });
+
+          // start damage
+          if (healthRef.current) clearInterval(healthRef.current);
+          healthRef.current = setInterval(() => {
+            setHealth((prev: number) => Math.max(0, prev - 5));
+          }, 1000 * 10);
+        } else if (nextAppState === "active") {
+          // stop damage
+          if (healthRef.current) clearInterval(healthRef.current);
+          // stop death timer
+          if (deathRef.current) clearInterval(deathRef.current);
+          // stop notificaiton timer
+          if (notificationRef.current) {
+            Notifications.cancelScheduledNotificationAsync(
+              notificationRef.current
+            );
+          }
+
+          setHealth((prev: number) => {
+            if (prev < 80 && state === TimerState.RUNNING) {
+              fetchQuote(mode, state, prev);
             }
-        };
-    }, [state, setHealth, setState, setTimeLeft, fetchQuote, mode, setExp]);
+            return prev;
+          });
+        }
+      }
+    );
+
+    return () => {
+      subscription.remove();
+      if (healthRef.current) clearInterval(healthRef.current);
+      if (deathRef.current) clearInterval(deathRef.current);
+      if (notificationRef.current) {
+        Notifications.cancelScheduledNotificationAsync(notificationRef.current);
+      }
+    };
+  }, [state, setHealth, setState, setTimeLeft, fetchQuote, mode, setExp]);
 };
